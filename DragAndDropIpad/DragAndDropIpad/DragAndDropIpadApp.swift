f790e868b1cfe4c152dd5ec4980089f5d00480ae
Antonio Aj <antonio.ajewole@hiyacar.co.uk>
2020-12-28 23:16:03 +0000

    Circle ci first run

---
+++ DragAndDropIpad/DragAndDropIpad/DragAndDropIpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropIpadApp.swift
+//  DragAndDropIpad
+//
+//  Created by Antonio Aj on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropIpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DragAndDropIpad/DragAndDropIpad/WelcomeViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  WelcomeViewModel.swift
+//  DragAndDropIpad
+//
+//  Created by Antonio Aj on 28/12/2020.
+//
+
+import Foundation
+class WelcomeViewModel {
+    let array = ["0", "1", "2", "10"]
+}
